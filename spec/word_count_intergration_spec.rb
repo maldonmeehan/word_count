79fc33328165c66a43341f792bf2c40a026776c4
Maldon <maldonmeehan@gmail.com>
2016-07-01 09:36:45 -0700

    processes the user inputed word and inputed sentence and returns the number of time the inputed word appears faling test for intergration_spec test

--- spec/word_count_intergration_spec.rb
+++ spec/word_count_intergration_spec.rb
@@ -2,3 +2,13 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe("the word cound results path", {:type => :feature}) do
+  it('processes the user inputed word and inputed sentence and returns the number of time the inputed word appears') do
+    visit('/')
+    fill_in('input_word', :with => "the")
+    fill_in('input_sentence', :with => "The quick brown fox jumped over the lazy sleeping dog.")
+    click_button("Test")
+    expect(page).to have_content('2')
+  end
+end
