138ebfd893d4e9bc05f1e1d84a88c937140bc0aa
Maldon <maldonmeehan@gmail.com>
2016-07-01 09:02:07 -0700

    counts how many time a word appears in a string when the user enters a capital letter failing test

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -5,4 +5,8 @@ describe('String#word_count') do
   it("counts how many time a word appears in a string") do
     expect(("hello").word_count("hello world")).to(eq(1))
   end
+
+  it("counts how many time a word appears in a string") do
+    expect(("the").word_count("The quick brown fox jumped over the lazy sleeping dog.")).to(eq(2))
+  end
 end
