f37b6b9e50c63b26481c961fd5fae83a3e0de70a
Maldon <maldonmeehan@gmail.com>
2016-07-01 08:03:44 -0700

    adds failing test one: counts how many time a letter appears in a string

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -1,2 +1,8 @@
 require('rspec')
 require('word_count')
+
+describe('String#word_count') do
+  it("counts how many time a letter appears in a string") do
+    expect(("a").word_count("abc")).to(eq("1"))
+  end
+end
