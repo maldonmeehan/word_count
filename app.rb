725b1419b7f258214795a370fe0c7c99e87fcae1
Maldon <maldonmeehan@gmail.com>
2016-07-01 09:28:47 -0700

    add index and results to app file

--- app.rb
+++ app.rb
@@ -2,3 +2,12 @@ require('sinatra')
 require('sinatra/reloader')
 require('./lib/word_count')
 also_reload('lib/**/*.rb')
+
+get('/') do
+  erb(:index)
+end
+
+get('/results') do
+  @results = @input_word.word_count(@input_sentence)
+  erb(:results)
+end
